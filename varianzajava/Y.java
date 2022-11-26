5fe90a1afd5d08222927010723f9a195229b3708
Giovanni-Boris <bancasi@unsa.edu.pe>
2022-11-26 06:28:46 -0500

    La contravarianza en java no es soportada el mensaje es distinoto al de java

--- varianzajava/Y.java
+++ varianzajava/Y.java
@@ -10,11 +10,19 @@ public class Y extends X {
     return obj;
     };*/
   //Convarainza si es soportada en lo que retorna si es aceptada
-  public C foo(B obj){
+  /*public C foo(B obj){
     obj.too();
     System.out.println("Foo desde covarianza en lo retorna Y");
     C obj2 = new ClaseC();
     return obj2;
+  };*/
+  //contravarianza no es sopoirtada Y cannot overide foo(B) in X
+  public A foo(B obj){
+    obj.too();
+    System.out.println("Foo desde contravarianza en lo que retorna Y");
+    B obj2 = new ClaseC();
+    return obj;
   };
- 
+
+
 }
