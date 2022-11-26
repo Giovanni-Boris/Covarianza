8cb577ee3b4b368b8319fa54ab06aa7fe786995c
Giovanni-Boris <bancasi@unsa.edu.pe>
2022-11-26 04:58:09 -0500

    Clase C para implementar interfaz C

---
+++ varianza++/ClaseC.cc
@@ -0,0 +1,12 @@
+#include "ClaseC.h"
+#include <iostream>
+
+void ClaseC::goo(){
+  std::cout<<"goo en claseC\n";
+}
+void ClaseC::too(){
+  std::cout<<"too en claseC\n";
+}
+void ClaseC::zoo(){
+  std::cout<<"zoo en claseC\n";
+}
---
+++ varianza++/ClaseC.h
@@ -0,0 +1,12 @@
+#ifndef CLASEC_H
+#define CLASEC_H
+#include "C.h"
+class ClaseC:public C {
+  public:
+    void goo();
+    void too();
+    void zoo();
+};
+
+
+#endif
