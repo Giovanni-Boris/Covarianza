0d018a9badecee3350b302e83d708d9707fb606d
Giovanni-Boris <bancasi@unsa.edu.pe>
2022-11-26 04:57:43 -0500

    ClaseB para implemetar interfazB

---
+++ varianza++/ClaseB.cc
@@ -0,0 +1,9 @@
+#include "ClaseB.h"
+#include <iostream>
+void ClaseB::too(){
+  std::cout<<"too en claseB\n";
+}
+void ClaseB::goo(){
+  std::cout<<"goo en CLaseB\n";
+}
+
---
+++ varianza++/ClaseB.h
@@ -0,0 +1,11 @@
+#ifndef CLASEB_H
+#define CLASEB_H
+#include "B.h"
+class ClaseB: public B {
+  public:
+    void too();
+    void goo();
+};
+
+
+#endif
