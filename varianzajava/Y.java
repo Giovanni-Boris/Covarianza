9700c9e17bc9cc4a59b17c6bea6ca2b12741aded
Giovanni-Boris <bancasi@unsa.edu.pe>
2022-11-26 06:00:29 -0500

    Usando prueba en foo retorna B acepta B

--- varianzajava/Y.java
+++ varianzajava/Y.java
@@ -8,29 +8,5 @@ public class Y extends X {
     System.out.println("Foo desde Y");
     return new ClaseB();
   };
-  /*@Override
-  public B foo(A obj){
-    obj.too();
-    System.out.println("Foo desde Y");
-    return new ClaseB();
-  };
-  @Override
-  public B foo(C obj){
-    obj.too();
-    System.out.println("Foo desde Y");
-    return new ClaseB();
-  };*/
-  /*public A foo(B obj){
-    obj.too();
-    System.out.println("Foo desde Y");
-    A obj1 = new ClaseB();
-    return obj1;
-  };/*
-  /*
-  @Override
-  public C foo(B obj){
-    obj.too();
-        
-    return new ClaseC();
-  };*/
+
 }
