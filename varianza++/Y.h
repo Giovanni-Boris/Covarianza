15a087dab2333e38e9635b0c79eff6993174214a
Giovanni-Boris <bancasi@unsa.edu.pe>
2022-11-26 05:07:13 -0500

    Probando return B y recibir B no dbe haber problemas

--- varianza++/Y.h
+++ varianza++/Y.h
@@ -8,44 +8,13 @@
 #include "ClaseC.h"
 class Y:public X {
   public:
-    /*B* goo(B *obj){
-      obj->goo();
-      B* obj2 = new ClaseC;
-      std::cout<<"FOO DESDE Y\n";
-      return obj2;
-    }*/
-    //covarainza si es soportdada
-    /*C* goo(B *obj){
-      obj->goo();
-      C* obj2 = new ClaseC;
-      std::cout<<"FOO DESDE Y\n";
-      return obj2;
-    }*/
-     // invalid contravarianza
-    A* goo(B *obj){
+    B* goo(B *obj){
       obj->goo();
       B* obj2 = new ClaseC;
       std::cout<<"FOO DESDE Y\n";
       return obj2;
     }
-    //Cvarainza para el elemento que recibe no es soportada
-    // si es soportada
-    /*B* goo(C *obj){
-      obj->goo();
-      B* obj2 = new ClaseC;
-      std::cout<<"FOO DESDE Y\n";
-      return obj2;
-    }*/
 
-    //contravarianza si es soportada
-    /*B* goo(A *obj){
-      B* obj2 = new ClaseC;
-      std::cout<<"FOO DESDE Y\n";
-      return obj2;
-    }*/
-    /*void print(){
-
-    }*/
 
 
 
