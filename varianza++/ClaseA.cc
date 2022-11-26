70775fd01316161ef24e7c7afd96cd8d01358fc7
Giovanni-Boris <bancasi@unsa.edu.pe>
2022-11-26 04:55:52 -0500

    Clase A para implemntar interfaz A

---
+++ varianza++/ClaseA.cc
@@ -0,0 +1,5 @@
+#include "ClaseA.h"
+#include <iostream>
+void ClaseA::too(){
+  std::cout<<"Too en la claseA\n";
+}
---
+++ varianza++/ClaseA.h
@@ -0,0 +1,12 @@
+#ifndef CLASEA_H
+#define CLASEA_H
+#include "A.h"
+
+class ClaseA: public A{
+  public:
+    void too();
+};
+
+
+#endif
+
