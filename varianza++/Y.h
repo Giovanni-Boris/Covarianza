0c57ac089a9c232eb8e642dade2ca3bddf8a4d57
Giovanni-Boris <bancasi@unsa.edu.pe>
2022-11-26 05:09:27 -0500

    Usando covarianza en el elemento que retorna c++ si lo soporta

--- varianza++/Y.h
+++ varianza++/Y.h
@@ -8,14 +8,20 @@
 #include "ClaseC.h"
 class Y:public X {
   public:
-    B* goo(B *obj){
+    /*B* goo(B *obj){
       obj->goo();
       B* obj2 = new ClaseC;
       std::cout<<"FOO DESDE Y\n";
       return obj2;
+    }*/
+    //covarainza si es soportdada
+    C* goo(B *obj){
+      obj->goo();
+      C* obj2 = new ClaseC;
+      std::cout<<"FOO DESDE usando convaraianza en el elemento que recibe Y\n";
+      return obj2;
     }
-
-
+    
 
 
 };
