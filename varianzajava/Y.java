8fe9bfa36707ec35ab2482e7787fd312a1a72d76
Giovanni-Boris <bancasi@unsa.edu.pe>
2022-11-26 06:29:56 -0500

    Covarainza en lo que recibe no es soportada o lo sobreescribe al igualqu e c++

--- varianzajava/Y.java
+++ varianzajava/Y.java
@@ -17,9 +17,16 @@ public class Y extends X {
     return obj2;
   };*/
   //contravarianza no es sopoirtada Y cannot overide foo(B) in X
-  public A foo(B obj){
+  /*public A foo(B obj){
     obj.too();
     System.out.println("Foo desde contravarianza en lo que retorna Y");
+    B obj2 = new ClaseC();
+    return obj;
+  };*/
+  //Covarainza para el elemento que recibe no es aceptado se sobreescribe el metodo
+  public B foo(C obj){
+    obj.too();
+    System.out.println("Foo desde  covarainza en lo que recibeY");
     B obj2 = new ClaseC();
     return obj;
   };
